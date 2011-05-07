63bd005111cbd8f4b2178e35ffaf51234cf2b553
akkana <akkana@shallowsky.com>
2011-05-07 20:36:55 +0000

    setup file, so we can also build RPM dists

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# http://docs.python.org/distutils/setupscript.html
+
+from distutils.core import setup
+
+setup(name='PyTopo',
+      version='1.0',
+      description='Online and Offline Maps',
+      author='Akkana Peck',
+      author_email='akkana@shallowsky.com',
+      url='http://shallowsky.com/software/topo/',
+      scripts=['pytopo'],
+      data_files=[ ('/usr/share/applications', ["pytopo.desktop"]),
+                   ('/usr/share/pixmaps',      ["pytopo.png"] )
+                 ]
+     )
+
