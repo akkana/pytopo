53b77553326609c00cbcea24faa9d1ffd20ab651
Akkana Peck <akkana@shallowsky.com>
2017-04-21 11:11:44 -0600

    Bump version to 1.5b1

--- pytopo/__init__.py
+++ pytopo/__init__.py
@@ -3,13 +3,13 @@
 '''pytopo module: display tiled maps from a variety of sources,
    along with trackpoints, waypoints and other useful information.
 
-   Copyright 2005 - 2016 by Akkana Peck, akkana@shallowsky.com
-   Please feel free to use, distribute or modify this program
-   under the terms of the GPL v2 or, at your option, a later GPL version.
+   Copyright 2005 - 2017 by Akkana Peck, akkana@shallowsky.com
+   Please use, distribute or modify this program under the terms
+   of the GPL v2 or, at your option, a later GPL version.
    I'd appreciate hearing about it if you make any changes.
 '''
 
-__version__ = "1.4"
+__version__ = "1.5b1"
 __author__ = "Akkana Peck <akkana@shallowsky.com>"
 __license__ = "GPL v2+"
 __all__ = ['Mapping', 'Cartography']
