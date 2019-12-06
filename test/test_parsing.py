789a3a7532467edcc78bf2fbab7669e1e3e8fae7
Akkana Peck <akkana@shallowsky.com>
2019-12-06 08:55:49 -0700

    Add some tests of trackstats and GPX parsing

--- test/test_parsing.py
+++ test/test_parsing.py
@@ -4,10 +4,13 @@ from __future__ import print_function
 
 import unittest
 import sys
+import datetime
 
 sys.path.insert(0, '..')
 
 from pytopo.MapViewer import parse_saved_site_line
+from pytopo.TrackPoints import TrackPoints, GeoPoint
+
 
 class ParseTests(unittest.TestCase):
 
@@ -15,11 +18,13 @@ class ParseTests(unittest.TestCase):
     def setUp(self):
         pass
 
+
     # executed after each test
     def tearDown(self):
         pass
 
-    def test_parsing(self):
+
+    def test_parse_saved_sites(self):
         lines = [
             '"San Francisco", -122.245, 37.471',
             'sydney, 151.125, -33.517, "", 11',
@@ -34,4 +39,35 @@ class ParseTests(unittest.TestCase):
         for i, line in enumerate(lines):
             parsed = parse_saved_site_line(line)
             self.assertEqual(parsed, expected[i])
+
+
+    def verify_otowi(self, trackpoints):
+        self.assertEqual(len(trackpoints.points), 1265)
+        self.assertAlmostEqual(trackpoints.minlon, -106.2534204)
+        self.assertAlmostEqual(trackpoints.maxlon, -106.2283611)
+        self.assertAlmostEqual(trackpoints.minlat, 35.8849806)
+        self.assertAlmostEqual(trackpoints.maxlat, 35.895508)
+
+        # Check points, the first label and the first real point
+        self.assertEqual(len(trackpoints.points), 1265)
+
+        self.assertIsInstance(trackpoints.points[0], str)
+        self.assertEqual(trackpoints.points[0], 'otowi-mesa-arch.gpx')
+
+        self.assertIsInstance(trackpoints.points[1], GeoPoint)
+        self.assertAlmostEqual(float(trackpoints.points[1].ele), 2108.0)
+        self.assertEqual(trackpoints.points[1].timestamp,
+                         '2016-03-02T17:28:45Z')
+
+        self.assertEqual(len(trackpoints.waypoints), 3)
+        lastpt = trackpoints.waypoints[-1]
+        self.assertEqual(lastpt.name, 'Arches')
+        self.assertAlmostEqual(lastpt.lat, 35.8872124)
+        self.assertAlmostEqual(lastpt.lon, -106.2297864)
+
+
+    def test_read_gpx(self):
+        trackpoints = TrackPoints()
+        trackpoints.read_track_file('test/data/otowi-mesa-arch.gpx')
+        self.verify_otowi(trackpoints)
 
---
+++ test/test_trackstats.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function
+
+import unittest
+import sys
+
+sys.path.insert(0, '..')
+
+import pytopo.TrackPoints
+import pytopo.trackstats
+
+class ParseTests(unittest.TestCase):
+
+    # executed prior to each test
+    def setUp(self):
+        pass
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+    def test_track_stats(self):
+        trackpoints = pytopo.TrackPoints()
+        trackpoints.read_track_file('test/data/otowi-mesa-arch.gpx')
+
+        halfwin = 0
+        beta = 2
+        metric = False
+
+        stats = pytopo.trackstats.statistics(trackpoints, halfwin, beta, metric)
+
+        self.assertAlmostEqual(stats['Total distance'], 5.150545737380824)
+        self.assertAlmostEqual(stats['Raw total climb'], 1345.1099999999979)
+        self.assertAlmostEqual(stats['Smoothed total climb'], 781.1629205918334)
+        self.assertAlmostEqual(stats['Lowest'], 6698.2771590538805)
+        self.assertAlmostEqual(stats['Highest'], 7070.277109582933)
+        self.assertAlmostEqual(stats['Moving time'], 13126)
+        self.assertAlmostEqual(stats['Stopped time'], 40656)
+        self.assertAlmostEqual(stats['Average moving speed'], 1.412613488844352)
+        self.assertEqual(len(stats['Distances']), 1263)
