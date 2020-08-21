dad81425ae4a9fb169681bab459d7b8e1d1073c6
Akkana Peck <akkana@shallowsky.com>
2020-08-21 16:23:12 -0600

    Bump version to 1.6.5 for packaging. Make sphinxdoc use the version from __init.py instead of maintaining its own.

--- pytopo/__init__.py
+++ pytopo/__init__.py
@@ -9,7 +9,7 @@
    I'd appreciate hearing about it if you make any changes.
 '''
 
-__version__ = "1.6.1"
+__version__ = "1.6.5"
 __author__ = "Akkana Peck <akkana@shallowsky.com>"
 __license__ = "GPL v2+"
 
