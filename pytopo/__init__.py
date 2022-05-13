ea2110e5dfae639c2d50f49e43f99d7899daca2b
Akkana Peck <akkana@shallowsky.com>
2022-05-12 20:09:30 -0600

    Bump version a little: getting close to a new release

--- pytopo/__init__.py
+++ pytopo/__init__.py
@@ -9,7 +9,7 @@
    I'd appreciate hearing about it if you make any changes.
 '''
 
-__version__ = "1.7.1pre1"
+__version__ = "1.7.1pre2"
 __author__ = "Akkana Peck <akkana@shallowsky.com>"
 __license__ = "GPL v2+"
 
